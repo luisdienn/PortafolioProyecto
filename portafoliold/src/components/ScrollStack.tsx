77921eb9a79a03e42bc37f2382ad4412508d8f9b
luisdienn <ldienb@ucenfotec.ac.cr>
2025-07-15 20:21:07 -0600

    CasiListo1.4-Responsive

--- portafoliold/src/components/ScrollStack.tsx
+++ portafoliold/src/components/ScrollStack.tsx
@@ -13,7 +13,7 @@ export const ScrollStackItem: React.FC<ScrollStackItemProps> = ({
   style,
 }) => (
   <div
-    className={`scroll-stack-card relative w-full h-80 my-8 p-12 rounded-[40px] shadow-[0_0_30px_rgba(0,0,0,0.1)] box-border origin-top will-change-transform ${itemClassName}`.trim()}
+    className={`scroll-stack-card relative w-full h-80 my-8 p-6 sm:p-12 rounded-[40px] shadow-[0_0_30px_rgba(0,0,0,0.1)] box-border origin-top will-change-transform ${itemClassName}`.trim()}
     style={{
       backfaceVisibility: "hidden",
       transformStyle: "preserve-3d",
